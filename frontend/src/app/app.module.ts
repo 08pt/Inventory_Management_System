f5d1c2ad92ee35715e4745c7477bb49159d7ff16
Priya Tiwari <87354985+08pt@users.noreply.github.com>
2022-06-06 00:19:58 +0530

    commit

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,22 +1,60 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CustomerComponent } from './customer/customer.component';
 import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './home/home.component';
+import { Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { SupplierComponent } from './supplier/supplier.component';
+import { ItemComponent } from './item/item.component';
+import { AdminComponent } from './admin/admin.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CustomersComponent } from './customers/customers.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { LoginComponent } from './login/login.component';
+
+const appRoutes: Routes = [
+
+  { path: 'admin', component: AdminComponent },
+  { path: 'admin', component: CustomersComponent },
+
+  { path: 'home', component: HomeComponent },
+  { path: 'item', component: ItemComponent},
+  { path: 'customer', component: CustomersComponent},
+
+
+
+]
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    CustomerComponent
+    CustomersComponent,
+    HomeComponent,
+    SupplierComponent,
+    ItemComponent,
+    AdminComponent,
+    HeaderComponent,
+    FooterComponent,
+    
+    DashboardComponent,
+    CustomersComponent,
+    RegistrationComponent,
+    LoginComponent,
+  
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
